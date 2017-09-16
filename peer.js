4d153a4e192b8eb43791f76ae1f4530977e86ab0
karan-garg <karangarg01001@gmail.com>
2017-09-16 15:39:36 -0400

    add TURN server

--- peer.js
+++ peer.js
@@ -1,26 +1,32 @@
-
 var peer;
 
-function listen(){
-    peer.on('connection', function(connection) {
-        connection.on('data', function(data){
+function listen() {
+    peer.on('connection', function (connection) {
+        connection.on('data', function (data) {
             // Will print 'hi!'
             console.log(data);
         });
     });
 }
 
-function registerPeer(){
+function registerPeer() {
     var myID = document.getElementById("myID").value || "hostPeer";
-    peer = new Peer(myID, {key: 'y22brty3oaiz4cxr'});
+    peer = new Peer(myID, {
+        key: 'y22brty3oaiz4cxr',
+        config: {
+            'iceServers': [
+                {url: 'numb.viagenie.ca', username: 'kgarg@uwaterloo.ca', credential: 'bigbang123'}
+            ]
+        }
+    });
     listen();
 }
 
-function connect(){
+function connect() {
     console.log("trying to connect");
     var targetPeerID = document.getElementById("targetPeerID").value;
     var connection = peer.connect(targetPeerID);
-    connection.on('open', function(){
+    connection.on('open', function () {
         connection.send('hi!');
         console.log("connection opened; message sent!")
     });
