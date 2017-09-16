096f5c55d327469062bf051f12c1577d37c4702c
karan-garg <karangarg01001@gmail.com>
2017-09-16 14:02:15 -0400

    set up peer.js

---
+++ peer.js
@@ -0,0 +1,30 @@
+
+var peer;
+
+function listen(){
+    peer.on('connection', function(connection) {
+        connection.on('data', function(data){
+            // Will print 'hi!'
+            console.log(data);
+        });
+    });
+}
+
+function registerPeer(){
+    var myID = document.getElementById("myID").value || "hostPeer";
+    peer = new Peer(myID, {key: 'y22brty3oaiz4cxr'});
+    listen();
+}
+
+function connect(){
+    console.log("trying to connect");
+    var targetPeerID = document.getElementById("targetPeerID").value;
+    var connection = peer.connect(targetPeerID);
+    connection.on('open', function(){
+        connection.send('hi!');
+        console.log("connection opened; message sent!")
+    });
+}
+
+document.getElementById("connectBtn").addEventListener("click", connect);
+document.getElementById("registerBtn").addEventListener("click", registerPeer);
